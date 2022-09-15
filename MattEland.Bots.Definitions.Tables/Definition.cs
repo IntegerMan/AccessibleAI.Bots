e9649bee0e028883030555daed817c37adc040d5
Matt Eland <matt.eland@gmail.com>
2022-09-15 00:15:14 -0400

    Working on componentizing these libraries.

--- MattEland.Bots.Definitions.Tables/Definition.cs
+++ MattEland.Bots.Definitions.Tables/Definition.cs
@@ -4,12 +4,38 @@ using Azure.Data.Tables;
 
 namespace MattEland.Bots.Definitions.Tables
 {
+    /// <summary>
+    /// Represents a definition from Azure Table Storage. This assumes that the Value and Related properties exist on the table.
+    /// </summary>
     public class Definition : ITableEntity
     {
-        public string Value { get; set; }
-        public string PartitionKey { get; set; }
-        public string RowKey { get; set; }
+        /// <summary>
+        /// Custom Value information. This is the main piece of data being retrieved.
+        /// </summary>
+        public string? Value { get; set; }
+
+        /// <summary>
+        /// Custom related information. Use this to link to additional information. This could be a URL or comma separated list of IDs or whatever else is convenient.
+        /// </summary>
+        public string? Related { get; set; }
+        /// <summary>
+        /// The partition key in the table
+        /// </summary>
+        public string? PartitionKey { get; set; }
+        
+        /// <summary>
+        /// The row key in the table
+        /// </summary>
+        public string? RowKey { get; set; }
+
+        /// <summary>
+        /// The timestamp for the row
+        /// </summary>
         public DateTimeOffset? Timestamp { get; set; }
+        
+        /// <summary>
+        /// The row's modification ETag
+        /// </summary>
         public ETag ETag { get; set; }
     }
 }
--- MattEland.Bots.Definitions.Tables/DefinitionRepository.cs
+++ MattEland.Bots.Definitions.Tables/DefinitionRepository.cs
@@ -3,25 +3,40 @@ using Azure.Data.Tables;
 
 namespace MattEland.Bots.Definitions.Tables
 {
+    /// <summary>
+    /// A repository for looking up information from Azure Table Storage
+    /// </summary>
     public class DefinitionRepository
     {
         private readonly TableClient _tableClient;
-        private readonly string _partitionKey;
+        private readonly string _defaultPartitionKey;
 
-        public DefinitionRepository(string storageConnStr, string tableName, string partitionKey)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefinitionRepository"/> class.
+        /// </summary>
+        /// <param name="storageConnStr">The Azure Storage connection string</param>
+        /// <param name="tableName">The name of the table within the Azure Storage account</param>
+        /// <param name="defaultPartitionKey">The partition key to use when none is specified in LookupDefinition</param>
+        public DefinitionRepository(string storageConnStr, string tableName, string defaultPartitionKey)
         {
             _tableClient = new TableClient(storageConnStr, tableName);
 
-            _partitionKey = partitionKey;
+            _defaultPartitionKey = defaultPartitionKey;
         }
 
-        public string LookupDefinition(string termKey)
+        /// <summary>
+        /// Gets the definition for the specified term key from Azure Table Storage using the repository's settings.
+        /// </summary>
+        /// <param name="rowKey">The row key to look up</param>
+        /// <param name="partitionKey">The partition key within the table. If none is specified, the default partition key will be used</param>
+        /// <returns>The definition or null</returns>
+        public Definition? LookupDefinition(string rowKey, string? partitionKey = null)
         {
-            Response<Definition> response = _tableClient.GetEntity<Definition>(_partitionKey, termKey);
+            partitionKey ??= _defaultPartitionKey;
 
-            Definition def = response.Value;
+            Response<Definition?> response = _tableClient.GetEntity<Definition?>(partitionKey, rowKey);
 
-            return def.Value;
+            return response.Value;
         }
     }
 }
