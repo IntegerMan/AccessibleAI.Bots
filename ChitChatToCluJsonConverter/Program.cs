bbf998f2a31a31fc1d35af86a0ade566013e0502
Matt Eland <matt.eland@gmail.com>
2022-09-18 13:08:39 -0400

    Progress on TSV conversion

--- ChitChatToCluJsonConverter/Program.cs
+++ ChitChatToCluJsonConverter/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace ChitChatToCluJsonConverter;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string csvFile = @"chitchat.tsv";
+        TextFieldParser parser = new(csvFile);
+        parser.TextFieldType = FieldType.Delimited;
+        parser.SetDelimiters("\t");
+
+        while (!parser.EndOfData)
+        {
+            string[] row = parser.ReadFields()!;
+
+            string utterance = row[0];
+            string intent = row[1];
+
+            Console.WriteLine($"{intent}: {utterance}");
+        }
+
+        // TODO: Write the JSON file
+    }
+}
