c127850bcbae00d68c853706607b6f400762d946
Matt Eland <matt.eland@gmail.com>
2022-09-22 17:24:51 -0400

    Working on Levenshtein file-based.

---
+++ AccessibleAI.Bots.Language.Levenshtein.Tests/ChitChatIntentMatchingTests.cs
@@ -0,0 +1,36 @@
+namespace AccessibleAI.Bots.Language.Levenshtein.Tests;
+
+public class ChitChatIntentMatchingTests
+{
+    [Theory]
+    [InlineData("Hello World", "Hi")]
+    //[InlineData("Hey there World", "Hi")]
+    [InlineData("Let's buy this car!", "BuyCar")]
+    //[InlineData("So long!", "Bye")]
+    public void CorrectIntentShouldBeDetected(string utterance, string intent)
+    {
+        // Arrange      
+        InMemoryLevenshteinEntityProvider provider = new(
+            new LevenshteinEntry[] {
+                new LevenshteinEntry("Hello", "Hi", "Test"),
+                new LevenshteinEntry("Hi", "Hi", "Test"),
+                new LevenshteinEntry("Hail and well met", "Hi", "Test"),
+                new LevenshteinEntry("Goodbye", "Bye", "Test"),
+                new LevenshteinEntry("Later!", "Bye", "Test"),
+                new LevenshteinEntry("See you later!", "Bye", "Test"),
+                new LevenshteinEntry("Bye for now!", "Bye", "Test"),
+                new LevenshteinEntry("Goodbye my friend", "Bye", "Test"),
+                new LevenshteinEntry("I want to buy a car", "BuyCar", "Test") 
+            });
+
+        LevenshteinIntentResolver resolver = new();
+        resolver.RegisterProvider(provider);
+
+        // Ac
+        IntentResolutionResult result = resolver.FindIntent(utterance);
+
+        // Assert
+        result.IntentName.ShouldBe(intent);
+        result.ConfidenceScore.ShouldBeGreaterThan(0);
+    }
+}
---
+++ AccessibleAI.Bots.Language.Levenshtein/LevenshteinStringBasedEntityProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AccessibleAI.Bots.Language.Levenshtein;
+
+public class LevenshteinStringBasedEntityProvider : ILevenshteinEntityProvider
+{
+    public LevenshteinStringBasedEntityProvider(string data)
+    {
+        Data = data;
+    }
+
+    public string[] Delimiters { get; set; } = new[] { "/t" };
+    public bool HasHeaderRow { get; set; } = true;
+    public string DefaultOrchestrationName { get; set; } = "None";
+    public string DefaultIntentName { get; set; } = "None";
+    public string Data { get; }
+
+    public IEnumerable<LevenshteinEntry> GetEntries()
+    {
+        using (StringReader reader = new(Data))
+        {
+            if (HasHeaderRow)
+            {
+                reader.ReadLine();
+            }
+
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+
+                string[] results = line.Split(Delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                string text = results[0];
+                string intentName = GetStringOrDefault(results, 1, DefaultIntentName);
+                string orchestrationName = GetStringOrDefault(results, 2, DefaultOrchestrationName);
+
+                yield return new LevenshteinEntry(text, intentName, orchestrationName);
+
+                line = reader.ReadLine();
+            }
+        }
+    }
+
+    private string GetStringOrDefault(string[] results, int index, string defaultValue) 
+        => results.Length > index ? results[index] : defaultValue;
+}
