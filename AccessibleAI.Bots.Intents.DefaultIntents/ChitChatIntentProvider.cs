6b49f7e653ca501482e0a41be8c19f4a2bd04a97
Matt Eland <matt.eland@gmail.com>
2022-10-04 16:25:09 -0400

    Working on testing things. Dependencies suck.

---
+++ AccessibleAI.Bots.Intents.DefaultIntents/ChitChatIntentProvider.cs
@@ -0,0 +1,17 @@
+﻿using AccessibleAI.Bots.Core.Language;
+using AccessibleAI.Bots.Language.Levenshtein;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessibleAI.Bots.Intents.DefaultIntents;
+
+public class ChitChatIntentResolver : LevenshteinTextFileEntityProvider
+{
+    public ChitChatIntentResolver(string filePath = "chitchat.tsv") : base(filePath)
+    {
+    }
+
+}
